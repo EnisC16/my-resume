4010b6a9e7bd79b09477feecdcb9f7d230d64b84
EnisC16 <enis.c16@outlook.com>
2021-02-06 21:56:04 +0100

    resume v1.0

---
+++ src/Components/Contact.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import emailjs from 'emailjs-com';
+
+const Contact = ({ data }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  if (data) {
+    var contactName = data.name;
+    var street = data.address.street;
+    var city = data.address.city;
+    var state = data.address.state;
+    var zip = data.address.zip;
+    var phone = data.phone;
+    var contactEmail = data.email;
+    var contactMessage = data.contactmessage;
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("e: ",e.target);
+    emailjs.sendForm('service_hcoxu7j', 'template_2ep82q9', '.contact__form', 'user_G975oItXwU30dkreGaOZ4')
+          .then((result) => {
+              console.log(result.text);
+              setIsSuccess(true);
+          }, (error) => {
+              console.log(error.text);
+              setIsError(true);
+          });
+  };
+
+  return (
+    <section id="contact">
+      <div className="row section-head">
+        <div className="two columns header-col">
+          <h1>
+            <span>Get In Touch.</span>
+          </h1>
+        </div>
+
+        <div className="ten columns">
+          <p className="lead">{contactMessage}</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="eight columns">
+          {(!isSuccess && !isError) ? (
+          <form onSubmit={submitForm} className="contact__form">
+            <fieldset>
+              <div>
+                <label htmlFor="contactName">
+                  Name <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  value={name}
+                  size="35"
+                  id="contactName"
+                  name="contactName"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactEmail">
+                  Email <span className="required">*</span>
+                </label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  value={email}
+                  size="35"
+                  id="contactEmail"
+                  name="contactEmail"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactSubject">Subject</label>
+                <input
+                  type="text"
+                  defaultValue=""
+                  value={subject}
+                  size="35"
+                  id="contactSubject"
+                  name="contactSubject"
+                  onChange={(e) => setSubject(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="contactMessage">
+                  Message <span className="required">*</span>
+                </label>
+                <textarea
+                  cols="50"
+                  rows="15"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  id="contactMessage"
+                  name="contactMessage"
+                ></textarea>
+              </div>
+
+              <div>
+                <button onClick={submitForm} type="submit" className="submit">
+                  Submit
+                </button>
+              </div>
+            </fieldset>
+          </form>
+          ) : (
+            (!isSuccess && isError) ? (
+              <div id="message-warning"> Some Error occured. Try it later or send an email via email provider. </div>
+            ) : (
+              <div id="message-success">
+                <i className="fa fa-check"></i>Your message was sent, thank you!
+                <br />
+              </div>
+            )
+          )}
+        </div>
+
+        <aside className="four columns footer-widgets">
+          <div className="widget widget_contact">
+            <h4>Address and Phone</h4>
+            <p className="address">
+              {contactName}
+              <br />
+              {contactEmail}
+              <br />
+              <br />
+              {street} <br />
+              {zip} {city}
+              <br />
+              <span>{phone}</span>
+            </p>
+          </div>
+        </aside>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
